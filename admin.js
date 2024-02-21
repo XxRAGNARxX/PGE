1e7756253c21b8135fa691e6223392fc689f70bf
XxRAGNARxX <gogogo0543@gmail.com>
2024-02-21 16:48:10 +0200

    login

--- admin.js
+++ admin.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import {getDatabase, ref, set,update,child} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAT9P-YtX3qerVIk2CxpiqKeyY9D6hD27U",
   authDomain: "databaseforlogin-e873f.firebaseapp.com",
@@ -18,52 +14,50 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
 // Initialize variables
 
-const auth = firebase.auth()
-const database = firebase.database()
+const auth = getAuth(firebase);
+const database = getDatabase(firebase);
 
+document.getElementById("loginbtn").onclick = login
+document.getElementById("regbtn").onclick = register
 // Set up our register function
-function register () {
+function register() {
   // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
-  full_name = document.getElementById('full_name').value
-  favourite_song = document.getElementById('favourite_song').value
-  milk_before_cereal = document.getElementById('milk_before_cereal').value
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+  const full_name = document.getElementById('full_name').value
 
   // Validate input fields
-  if (validate_email(email) == false || validate_password(password) == false) {
+  if (!validate_email(email) || !
+  validate_password(password)) {
     alert('Email or Password is Outta Line!!')
     return
     // Don't continue running the code
   }
-  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
-    alert('One or More Extra Fields is Outta Line!!')
-    return
-  }
+  //if (!validate_field(full_name) || !validate_field(favourite_song) || !validate_field(milk_before_cereal)) {
+  //  alert('One or More Extra Fields is Outta Line!!')
+  //  return
+  //}
  
   // Move on with Auth
-  auth.createUserWithEmailAndPassword(email, password)
-  .then(function() {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(function(userCredentials) {
     // Declare user variable
-    var user = auth.currentUser
+    var user =  userCredentials.user;
 
     // Add this user to Firebase Database
-    var database_ref = database.ref()
+    var database_ref = ref(database, "users/"+user.uid)
 
     // Create User data
     var user_data = {
       email : email,
       full_name : full_name,
-      favourite_song : favourite_song,
-      milk_before_cereal : milk_before_cereal,
-      last_login : Date.now()
     }
 
     // Push to Firebase Database
-    database_ref.child('users/' + user.uid).set(user_data)
+    set(database_ref, user_data)
 
     // DOne
     alert('User Created!!')
@@ -78,10 +72,10 @@ function register () {
 }
 
 // Set up our login function
-function login () {
+function login() {
   // Get all our input fields
-  email = document.getElementById('email').value
-  password = document.getElementById('password').value
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
 
   // Validate input fields
   if (validate_email(email) == false || validate_password(password) == false) {
@@ -90,13 +84,13 @@ function login () {
     // Don't continue running the code
   }
 
-  auth.signInWithEmailAndPassword(email, password)
+  signInWithEmailAndPassword(auth, email, password)
   .then(function() {
     // Declare user variable
     var user = auth.currentUser
 
     // Add this user to Firebase Database
-    var database_ref = database.ref()
+    var database_ref = ref(database, "users/"+user.uid)
 
     // Create User data
     var user_data = {
@@ -104,7 +98,7 @@ function login () {
     }
 
     // Push to Firebase Database
-    database_ref.child('users/' + user.uid).update(user_data)
+    update(database_ref, user_data)
 
     // DOne
     alert('User Logged In!!')
@@ -124,7 +118,7 @@ function login () {
 
 // Validate Functions
 function validate_email(email) {
-  expression = /^[^@]+@\w+(\.\w+)+\w$/
+  const expression = /^[^@]+@\w+(\.\w+)+\w$/
   if (expression.test(email) == true) {
     // Email is good
     return true
@@ -150,7 +144,6 @@ function validate_field(field) {
 
   if (field.length <= 0) {
     return false
-  } else {
-    return true
   }
+  return true
 }
